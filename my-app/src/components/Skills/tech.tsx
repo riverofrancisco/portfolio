a4a3875d940d6c6d254dcf53dfa83ae5727f40d6
riverofrancisco <francisco.rivero124@gmail.com>
2023-03-20 21:18:53 -0300

    Addingn TechSkills

--- my-app/src/components/Skills/tech.tsx
+++ my-app/src/components/Skills/tech.tsx
@@ -1,1 +1,32 @@
 import React from "react";
+import Card from "./skillCard";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import { Skill, techSkills } from "../../data/data";
+import Typography from "@mui/material/Typography";
+
+const TechSkills = () => {
+  return (
+    <Grid container spacing={10} mt={2} borderTop={3} borderBottom={3}>
+      <Grid
+        item
+        xs={6}
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        border={1}
+      >
+        <Typography variant="h1">TechSkills</Typography>
+      </Grid>
+      <Grid item xs={6} display="flex" border={1}>
+        <Box width="100%" display="flex" flexWrap="wrap">
+          {techSkills.map((sk: Skill) => {
+            return <Card key={sk.name} skill={sk} />;
+          })}
+        </Box>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default TechSkills;
--- my-app/src/router/index.tsx
+++ my-app/src/router/index.tsx
@@ -4,12 +4,14 @@ import { Routes, Route } from "react-router-dom";
 import LandingPage from "../components/Landing/Landing";
 import NavBar from "../components/Nav/Navbar";
 import SoftSkills from "../components/Skills/soft";
+import TechSkills from "../components/Skills/tech";
 
 export const AppRouter = () => {
   return (
     <div>
       <NavBar />
       <LandingPage />
+      <TechSkills />
       <SoftSkills />
       <Routes></Routes>
     </div>
