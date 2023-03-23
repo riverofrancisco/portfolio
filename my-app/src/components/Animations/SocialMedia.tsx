a5be483968ab1d0c3a558d9494c6a54f550a794e
riverofrancisco <francisco.rivero124@gmail.com>
2023-03-22 21:21:55 -0300

    Speed Dial Working OK

--- my-app/src/components/Animations/SocialMedia.tsx
+++ my-app/src/components/Animations/SocialMedia.tsx
@@ -8,26 +8,45 @@ import EmailIcon from "@mui/icons-material/Email";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import SendIcon from "@mui/icons-material/Send";
+import { Link } from "@mui/material";
 
-const StyledSpeedDial: any = styled(SpeedDial)(({ theme }) => ({
-  position: "absolute",
-  "&.MuiSpeedDial-directionUp, &.MuiSpeedDial-directionLeft": {
-    bottom: theme.spacing(2),
-    right: theme.spacing(2),
-  },
-  "&.MuiSpeedDial-directionDown, &.MuiSpeedDial-directionRight": {
-    top: theme.spacing(2),
-    left: theme.spacing(2),
-  },
-}));
+const StyledSpeedDial: React.ElementType<SpeedDialProps> = styled(SpeedDial)(
+  ({ theme }) => ({
+    position: "absolute",
+    "&.MuiSpeedDial-directionUp, &.MuiSpeedDial-directionLeft": {
+      bottom: theme.spacing(2),
+      right: theme.spacing(2),
+    },
+    "&.MuiSpeedDial-directionDown, &.MuiSpeedDial-directionRight": {
+      top: theme.spacing(2),
+      left: theme.spacing(2),
+    },
+  })
+);
 
 const actions = [
-  { icon: <LinkedInIcon />, name: "LinkedIn" },
-  { icon: <GitHubIcon />, name: "GitHub" },
-  { icon: <EmailIcon />, name: "Email" },
+  {
+    icon: <LinkedInIcon />,
+    name: "LinkedIn",
+    linkto: "https://www.linkedin.com/in/rivero-francisco/",
+  },
+  {
+    icon: <GitHubIcon />,
+    name: "GitHub",
+    linkto: "https://github.com/riverofrancisco",
+  },
+  {
+    icon: <EmailIcon />,
+    name: "Email",
+    linkto: "mailto:franciscojose.rivero.ar@gmail.com",
+  },
 ];
 
 export default function SocialMediaSpeedDial() {
+  const handleLink = (link: string) => {
+    if (link) window.open(link, "_blank");
+  };
+
   return (
     <Box sx={{ transform: "translateZ(0px)", flexGrow: 1 }}>
       <StyledSpeedDial
@@ -37,9 +56,9 @@ export default function SocialMediaSpeedDial() {
       >
         {actions.map((action) => (
           <SpeedDialAction
-            key={action.name}
             icon={action.icon}
             tooltipTitle={action.name}
+            onClick={() => handleLink(action.linkto)}
           />
         ))}
       </StyledSpeedDial>
